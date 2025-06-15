4f69bb60314c3f36423db99bde875f4a62c3e34a
Abdullah Maajid <abdullahmaajid@MacBook-Pro-Abdullah.local>
2025-06-15 12:55:38 +0700

    api : logout, create, update, delete data

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -6,12 +6,10 @@ use App\Http\Controllers\Controller;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    /*
-     * Login user dengan email dan password.
-     */
     public function login(Request $request)
     {
         $data = $request->validate([
@@ -22,49 +20,72 @@ class AuthController extends Controller
         if (empty($data['email']) || empty($data['password'])) {
             return response()->json([
                 'status_code' => 400,
-                'message' => 'Email dan password harus diisi.',
+                'message' => 'Email dan password harus diisi',
             ], 400);
         }
 
         try {
-            if (!$token = Auth::guard('api')->attempt($data)) {
+            if (! $token = Auth::guard('api')->attempt($data)) {
                 return response()->json([
                     'status_code' => 401,
-                    'message' => 'Email atau password salah.',
+                    'message' => 'Email atau password salah',
                 ], 401);
             }
 
             $user = Auth::guard('api')->user();
             return response()->json([
                 'status_code' => 200,
-                'message' => 'Login berhasil.',
+                'message' => 'Login berhasil',
                 'data' => [
-                    'user' => [
-                        'id' => $user->id,
-                        'name' => $user->name,
-                        'email' => $user->email,
-                        'is_admin' => $user->is_admin,
-                    ],
-                    'token' => $token,
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'is_admin' => $user->is_admin,
                 ],
+                'token' => $token,
             ], 200);
-
         } catch (Exception $e) {
             return response()->json([
                 'status_code' => 500,
-                'message' => 'Terjadi kesalahan.',
+                'message' => 'Terjadi kesalahan',
             ], 500);
         }
     }
 
-    /*
-     * Logout user yang sedang login.
+    
+    /**
+     * @group Auth
+     *
+     * Logout user dari sistem.
+     *
+     * @authenticated
+     *
+     * @response 200 {
+     *   "status_code": 200,
+     *   "message": "Logout berhasil. Token telah dihapus."
+     * }
+     *
+     * @response 500 {
+     *   "status_code": 500,
+     *   "message": "Gagal logout, terjadi kesalahan."
+     * }
      */
-    public function logout()
+
+    public function logout(Request $request)
     {
-        Auth::guard('api')->logout();
-        return response()->json([
-            'message' => 'Logout berhasil.',
-        ], 200);
+        try {
+            JWTAuth::invalidate(JWTAuth::getToken());
+
+            return response()->json([
+                'status_code' => 200,
+                'message' => 'Logout berhasil. Token telah dihapus.',
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'Gagal logout, terjadi kesalahan.',
+            ], 500);
+        }
     }
+
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Gate;
@@ -13,28 +15,27 @@ use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /*
-     * Register any application services.
-     */
+    
     public function register(): void
     {
         //
     }
 
-    /*
-     * Bootstrap any application services.
-     */
+   
     public function boot(): void
     {
         Paginator::useTailwind();
-        
+
         Gate::define('admin', function ($user) {
             return $user->is_admin == true;
         });
 
         Scramble::configure()->routes(function (Route $route) {
-        return Str::startsWith($route->uri, 'api/');
+            return Str::startsWith($route->uri, 'api/');
+        })->withDocumentTransformers(function(OpenApi $openApi) {
+            $openApi->secure(
+                SecurityScheme::http('bearer')
+            );
         });
-
     }
 }
